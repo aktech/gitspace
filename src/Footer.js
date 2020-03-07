af597bdb82c16e9fafa810229f3aef9fd51093bf
Amit Kumar <dtu.amit@gmail.com>
2020-03-07 18:53:26 +0000

    Fix http -> https links

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
     render() {
         return (
             <footer>
-                <p>Source code at <a className="footer-text" href="http://github.com/aktech/">Github</a> • Made with 🖤 by <a className="footer-text" href="http://github.com/aktech/">Amit</a></p>
+                <p>Source code at <a className="footer-text" href="https://github.com/aktech/gitspace">Github</a> • Made with 🖤 by <a className="footer-text" href="https://iamit.in/">Amit</a></p>
             </footer>
         )
     }
