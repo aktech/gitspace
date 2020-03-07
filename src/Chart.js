4ede994fd2b5c654200f9ad3111210ac81240f3a
Amit Kumar <dtu.amit@gmail.com>
2020-03-07 13:59:30 +0000

    Minor fixes

--- src/Chart.js
+++ src/Chart.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react'
 import { ResponsiveBar } from '@nivo/bar'
 
+// These two values must be close enough for the size label to be placed correctly
+const yAxisDiff = 64
+const barHeight = 70
+
 const theme = {
     axis: {
         tickColor: "#eee",
@@ -32,15 +36,46 @@ class Chart extends Component {
         }
     }
 
+    getAllBars = (bars) => {
+        const remainingSize = this.props.data.length - bars.length
+        const dataToFill = this.props.data.slice(0, remainingSize)
+        let lastBar = bars[0]
+        let remainingElements = []
+        console.log(dataToFill)
+        dataToFill.reverse()
+        dataToFill.forEach(element => {
+            remainingElements.push({
+                    key: element.repoName,
+                    data: {
+                        id: "size", value: element.size, index: 20, indexValue: element.repoName,
+                        data: {
+                            repoName: element.repoName,
+                            size: element.size
+                        }
+                    },
+                    x: 0,
+                    y: lastBar.y + yAxisDiff,
+                    width: 1,
+                    height: 58,
+                    color: "#e8c1a0"
+                }
+            )
+            lastBar = remainingElements[remainingElements.length - 1]
+        })
+        return remainingElements.concat(bars)
+
+    }
 
     LabelText = ({ bars }) => {
-        let labels = bars.map(({ key, x, y, width, height }, index) => {
+        const barsAppended = this.getAllBars(bars)
+        console.log(barsAppended)
+        let labels = barsAppended.map(({ key, x, y, width, height }, index) => {
             return (
                 <text textAnchor="middle" x={width+50} y={y + height/2.1} style={{
-                    "font-size": "14px",
-                    "font-family": "'Slabo 27px', serif"
-                }}>
-                    {bars[index].data.value} MiB
+                    "fontSize": "14px",
+                    "fontFamily": "'Slabo 27px', serif"
+                }} key={index}>
+                    {barsAppended[index].data.value} MiB
                 </text>
             )
         })
@@ -50,7 +85,7 @@ class Chart extends Component {
     render() {
 
         return (
-        <div style={{height: Math.max(700, this.props.repos.length * 70)}} className="bar-chart">
+        <div style={{height: Math.max(700, this.props.repos.length * barHeight)}} className="bar-chart">
             <ResponsiveBar
                 data={
                     this.props.data
