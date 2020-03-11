b7be4922bdcbb680ed30d4df05a863804b1a9adb
Amit Kumar <dtu.amit@gmail.com>
2020-03-11 20:12:24 +0000

    Add Sentry for error tracking

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import * as Sentry from '@sentry/browser';
 import Form from "./Form";
 import Footer from "./Footer";
+
+Sentry.init({dsn: "https://f624cc79b0aa4acc97959177183f0d11@sentry.io/4400890"});
 
 class App extends Component {
     render() {
